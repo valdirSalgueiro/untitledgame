ff79acb4b2759dc7ed358d3535b0a2fe40993c81
Valdir Salgueiro <valdir.salgueiro@arizona.global>
2019-10-30 09:21:55 -0300

    remove moon

--- src/3d/Planets.js
+++ src/3d/Planets.js
@@ -2,11 +2,10 @@ import * as THREE from 'three'
 import { useLoader } from 'react-three-fiber'
 import React, { useRef } from 'react'
 import earthImg from '../images/earth.jpg'
-import moonImg from '../images/moon.png'
 
 export default function Planets() {
   const ref = useRef()
-  const [texture] = useLoader(THREE.TextureLoader, [earthImg, moonImg])
+  const [texture] = useLoader(THREE.TextureLoader, [earthImg])
   return (
     <group position={[0, 0, 0]} ref={ref} scale={[100, 100, 100]}>
       <mesh>
