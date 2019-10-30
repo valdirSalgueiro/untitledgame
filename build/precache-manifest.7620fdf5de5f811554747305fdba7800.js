f7a7eda6a2ccbd98d22d4947883f4427cb624a76
Valdir Salgueiro <valdir.salgueiro@arizona.global>
2019-10-30 09:20:38 -0300

    gitignore update

---
+++ build/precache-manifest.7620fdf5de5f811554747305fdba7800.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f6c865a6e7cd822c6558319e8e1799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8235297d5cf45b48fcca",
+    "url": "/static/js/2.5ceac5b7.chunk.js"
+  },
+  {
+    "revision": "066cfc43626b521d65e1",
+    "url": "/static/js/main.089accd6.chunk.js"
+  },
+  {
+    "revision": "4fbae370275d0948bd26",
+    "url": "/static/js/runtime-main.24c9291d.js"
+  },
+  {
+    "revision": "8d516cce9b80ee257214da40ace5c474",
+    "url": "/static/media/click.8d516cce.mp3"
+  },
+  {
+    "revision": "7cddb76f657dc2f936c1c97593007ba3",
+    "url": "/static/media/earth.7cddb76f.jpg"
+  },
+  {
+    "revision": "e896d081c42d144db443a0faeea36cf8",
+    "url": "/static/media/engine.e896d081.mp3"
+  },
+  {
+    "revision": "bdb080913490e33da93bb6510d530d4d",
+    "url": "/static/media/engine2.bdb08091.mp3"
+  },
+  {
+    "revision": "5c5ce6974ee4192d9ecbc74242daa4ce",
+    "url": "/static/media/explosion.5c5ce697.mp3"
+  },
+  {
+    "revision": "a97c8e670e85efbd49691d7e343da830",
+    "url": "/static/media/laser.a97c8e67.mp3"
+  },
+  {
+    "revision": "37071536c8af0b93df3ef69ebbd63e9c",
+    "url": "/static/media/moon.37071536.png"
+  },
+  {
+    "revision": "5699f1c4aff5845551ef0ba1a7ad31f8",
+    "url": "/static/media/warp.5699f1c4.mp3"
+  }
+]);
